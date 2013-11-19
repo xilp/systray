54e403c2200665c95772105c3aa21c609b8871e7
xilp <rorschach.c.liu@gmail.com>
2013-11-19 23:29:18 +0800

    Fixed bugs on win

--- example/main.go
+++ example/main.go
@@ -13,13 +13,6 @@ func main() {
 	}
 
 	tray := systray.New(os.Args[1], os.Args[2], 6333)
-	go func() {
-		err := tray.Run()
-		if err != nil {
-			println(err.Error())
-		}
-	}()
-
 	tray.OnClick(func() {
 		println("clicked")
 	})
@@ -28,22 +21,30 @@ func main() {
 		println(err.Error())
 	}
 
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		println("Input icon file name:")
-		print(">> ")
-		data, _, _ := reader.ReadLine()
-		line := string(data)
-		if len(line) == 0 {
-			break;
+	go func() {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			println("Input icon file name:")
+			print(">> ")
+			data, _, _ := reader.ReadLine()
+			line := string(data)
+			if len(line) == 0 {
+				break
+			}
+			err := tray.Show(line, line)
+			if err != nil {
+				println(err.Error())
+			}
 		}
-		err := tray.Show(line, line)
+
+		err = tray.Stop()
 		if err != nil {
 			println(err.Error())
 		}
-	}
+		os.Exit(0)
+	}()
 
-	err = tray.Stop()
+	err = tray.Run()
 	if err != nil {
 		println(err.Error())
 	}
--- tray_windows.go
+++ tray_windows.go
@@ -7,141 +7,39 @@ import (
 	"syscall"
 )
 
-func (p *_Systray) Run() error {
-	proc := func(hwnd HWND, msg uint32, wparam, lparam uintptr) uintptr {
-		if lparam == WM_LBUTTONDBLCLK {
-			p.dclick()
-		} else if (lparam == WM_LBUTTONUP) {
-			p.lclick()
-		} else if (lparam ==  WM_RBUTTONUP) {
-			p.rclick()
-		}
-		result, _, _ := DefWindowProc.Call(uintptr(hwnd), uintptr(msg), wparam, lparam)
-		return result
-	}
-
-	name := syscall.StringToUTF16Ptr("Systray")
-
-	hinst, _, _ := GetModuleHandle.Call(0)
-	if (hinst == 0) {
-		return errors.New("can't get module handle")
-	}
-	cursor, _, _ := LoadCursor.Call(0, IDC_ARROW)
-	if (cursor == 0) {
-		return errors.New("can't get cursor")
-	}
-	icon, _, _ := LoadIcon.Call(0, uintptr(IDI_APPLICATION))
-	if icon == 0 {
-		return errors.New("can't get app icon")
-	}
-
-	var wclass WNDCLASSEX
-	wclass.CbSize = uint32(unsafe.Sizeof(wclass))
-	wclass.LpfnWndProc = syscall.NewCallback(proc)
-	wclass.HInstance = HINSTANCE(hinst)
-	wclass.LpszClassName = name
-	wclass.HbrBackground = COLOR_WINDOW + 1
-	wclass.HCursor = HCURSOR(cursor)
-	wclass.HIcon = HICON(icon)
-
-	result, _, _ := RegisterClassEx.Call(uintptr(unsafe.Pointer(&wclass)))
-	if (result == 0) {
-		return errors.New("reg win class failed")
-	}
-	desktop, _, _ := GetDesktopWindow.Call()
-	if (desktop == 0) {
-		return errors.New("get desktop failed")
-	}
-
-	p.hwin, _, _ = CreateWindowEx.Call(
-		WS_EX_APPWINDOW,
-		uintptr(unsafe.Pointer(name)),
-		uintptr(unsafe.Pointer(name)),
-		WS_OVERLAPPEDWINDOW ,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		desktop,
-		0,
-		hinst,
-		0)
-
-	if (p.hwin == 0) {
-		return errors.New("create win failed")
-	}
-
-	msg := uintptr(unsafe.Pointer(&MSG{}))
-	for {
-		result, _, _ := GetMessage.Call(msg, 0, 0, 0)
-		if (result == 0) {
-			break
-		}
-		TranslateMessage.Call(msg);
-		DispatchMessage.Call(msg);
-	}
-	return nil
-}
-
 func (p *_Systray) Stop() error {
-	if (p.nid == nil) {
-		return nil
+	nid := NOTIFYICONDATA{
+		UID:  p.id,
+		HWnd: HWND(p.hwnd),
 	}
-	result, _, _ := Shell_NotifyIcon.Call(NIM_DELETE, uintptr(unsafe.Pointer(p.nid)))
-	if (result == 0) {
-		return errors.New("hide icon failed")
+	nid.CbSize = uint32(unsafe.Sizeof(nid))
+
+	ret, _, _ := Shell_NotifyIcon.Call(NIM_DELETE, uintptr(unsafe.Pointer(&nid)))
+	if ret == 0 {
+		return errors.New("shell notify delete failed")
 	}
 	return nil
 }
 
 func (p *_Systray) Show(file string, hint string) error {
 	path := filepath.Join(p.iconPath, file)
-	icon, _, _ := LoadImage.Call(
-		0,
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(path))),
-		IMAGE_ICON,
-		0,
-		0,
-		LR_LOADFROMFILE | LR_DEFAULTSIZE)
-
-	if (icon == 0) {
-		icon, _, _ = LoadIcon.Call(0, IDI_APPLICATION)
+	path, err := filepath.Abs(path)
+	if err != nil {
+		return err
 	}
-	if (icon == 0) {
-		return errors.New("can't load icon")
+	icon, err := NewIconFromFile(path)
+	if err != nil {
+		return err
 	}
-
-	flag := uint32(NIF_ICON | NIF_MESSAGE)
-	if len(hint) != 0 {
-		flag = flag | NIF_TIP
+	err = p.SetIcon(HICON(icon))
+	if err != nil {
+		return err
 	}
-
-	var nid *NOTIFYICONDATA
-	if (p.nid != nil) {
-		nid = p.nid
-	} else {
-		nid = &NOTIFYICONDATA{}
-		nid.CbSize = uint32(unsafe.Sizeof(nid))
-		nid.HWnd = HWND(p.hwin)
-		nid.UID = 1
-		nid.UCallbackMessage = WM_TRAYICON
+	err = p.SetTooltip(hint)
+	if err != nil {
+		return err
 	}
-
-	nid.UFlags = flag
-	nid.HIcon = HICON(icon)
-	copy(nid.SzTip[:], syscall.StringToUTF16(hint))
-
-	var result uintptr
-	if (p.nid != nil) {
-		result, _, _ = Shell_NotifyIcon.Call(NIM_MODIFY, uintptr(unsafe.Pointer(nid)))
-	} else {
-		result, _, _ = Shell_NotifyIcon.Call(NIM_ADD, uintptr(unsafe.Pointer(nid)))
-		p.nid = nid
-	}
-	if (result == 0) {
-		return errors.New("shell show call failed")
-	}
-	return nil
+	return p.SetVisible(true)
 }
 
 func (p *_Systray) OnClick(fun func()) {
@@ -150,18 +48,237 @@ func (p *_Systray) OnClick(fun func()) {
 	p.dclick = fun
 }
 
-func _NewSystray(iconPath string, clientPath string, port int) _Systray {
-	return _Systray{iconPath, 0, nil, func(){}, func(){}, func(){}}
+func (p *_Systray) SetTooltip(tooltip string) error {
+	nid := NOTIFYICONDATA{
+		UID:  p.id,
+		HWnd: HWND(p.hwnd),
+	}
+	nid.CbSize = uint32(unsafe.Sizeof(nid))
+
+	nid.UFlags = NIF_TIP
+	copy(nid.SzTip[:], syscall.StringToUTF16(tooltip))
+
+	ret, _, _ := Shell_NotifyIcon.Call(NIM_MODIFY, uintptr(unsafe.Pointer(&nid)))
+	if ret == 0 {
+		return errors.New("shell notify tooltip failed")
+	}
+	return nil
+}
+
+func (p *_Systray) SetVisible(visible bool) error {
+	nid := NOTIFYICONDATA{
+		UID:  p.id,
+		HWnd: HWND(p.hwnd),
+	}
+	nid.CbSize = uint32(unsafe.Sizeof(nid))
+
+	nid.UFlags = NIF_STATE
+	nid.DwStateMask = NIS_HIDDEN
+	if !visible {
+		nid.DwState = NIS_HIDDEN
+	}
+
+	ret, _, _ := Shell_NotifyIcon.Call(NIM_MODIFY, uintptr(unsafe.Pointer(&nid)))
+	if ret == 0 {
+		return errors.New("shell notify tooltip failed")
+	}
+	return nil
+}
+
+func (p *_Systray) SetIcon(hicon HICON) error {
+	nid := NOTIFYICONDATA{
+		UID:  p.id,
+		HWnd: HWND(p.hwnd),
+	}
+	nid.CbSize = uint32(unsafe.Sizeof(nid))
+
+	nid.UFlags = NIF_ICON
+	if hicon == 0 {
+		nid.HIcon = 0
+	} else {
+		nid.HIcon = hicon
+	}
+
+	ret, _, _ := Shell_NotifyIcon.Call(NIM_MODIFY, uintptr(unsafe.Pointer(&nid)))
+	if ret == 0 {
+		return errors.New("shell notify icon failed")
+	}
+	return nil
+}
+
+func (p *_Systray) WinProc(hwnd HWND, msg uint32, wparam, lparam uintptr) uintptr {
+	if msg == NotifyIconMessageId {
+		if lparam == WM_LBUTTONDBLCLK {
+			p.dclick()
+		} else if (lparam == WM_LBUTTONUP) {
+			p.lclick()
+		} else if (lparam ==  WM_RBUTTONUP) {
+			p.rclick()
+		}
+	}
+	result, _, _ := DefWindowProc.Call(uintptr(hwnd), uintptr(msg), wparam, lparam)
+	return result
+}
+
+func (p *_Systray) Run() error {
+	hwnd := p.mhwnd
+	var msg MSG
+	for {
+		rt, _, _ := GetMessage.Call(uintptr(unsafe.Pointer(&msg)), 0, 0, 0)
+		switch int(rt) {
+		case 0:
+			return nil
+		case -1:
+			return errors.New("run failed")
+		}
+
+		is, _, _ := IsDialogMessage.Call(hwnd, uintptr(unsafe.Pointer(&msg)))
+		if is == 0 {
+			TranslateMessage.Call(uintptr(unsafe.Pointer(&msg)))
+			DispatchMessage.Call(uintptr(unsafe.Pointer(&msg)))
+		}
+	}
+	return nil
+}
+
+func _NewSystray(iconPath string, clientPath string, port int) *_Systray {
+	tray, err := _NewSystrayEx(iconPath)
+	if err != nil {
+		panic(err)
+	}
+	return tray
+}
+
+func _NewSystrayEx(iconPath string) (*_Systray, error) {
+	ni := &_Systray{iconPath, 0, 0, 0, func(){}, func(){}, func(){}}
+
+	MainClassName := "MainForm"
+	RegisterWindow(MainClassName, ni.WinProc)
+
+	mhwnd, _, _ := CreateWindowEx.Call(
+		WS_EX_CONTROLPARENT,
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(MainClassName))),
+		0,
+		WS_OVERLAPPEDWINDOW|WS_CLIPSIBLINGS,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		0)
+	if mhwnd == 0 {
+		return nil, errors.New("create main win failed")
+	}
+
+	NotifyIconClassName := "NotifyIconForm"
+	RegisterWindow(NotifyIconClassName, ni.WinProc)
+
+	hwnd, _, _ := CreateWindowEx.Call(
+		0,
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(NotifyIconClassName))),
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		uintptr(HWND_MESSAGE),
+		0,
+		0,
+		0)
+	if hwnd == 0 {
+		return nil, errors.New("create notify win failed")
+	}
+
+	nid := NOTIFYICONDATA{
+		HWnd:             HWND(hwnd),
+		UFlags:           NIF_MESSAGE | NIF_STATE,
+		DwState:          NIS_HIDDEN,
+		DwStateMask:      NIS_HIDDEN,
+		UCallbackMessage: NotifyIconMessageId,
+	}
+	nid.CbSize = uint32(unsafe.Sizeof(nid))
+
+	ret, _, _ := Shell_NotifyIcon.Call(NIM_ADD, uintptr(unsafe.Pointer(&nid)))
+	if ret == 0 {
+		return nil, errors.New("shell notify create failed")
+	}
+
+	nid.UVersion = NOTIFYICON_VERSION
+
+	ret, _, _ = Shell_NotifyIcon.Call(NIM_SETVERSION, uintptr(unsafe.Pointer(&nid)))
+	if ret == 0 {
+		return nil, errors.New("shell notify version failed")
+	}
+
+	ni.id = nid.UID
+	ni.mhwnd = mhwnd
+	ni.hwnd = hwnd
+
+	return ni, nil
 }
 
 type _Systray struct {
 	iconPath string
-	hwin uintptr
-	nid *NOTIFYICONDATA
+	id uint32
+	mhwnd uintptr
+	hwnd uintptr
 	lclick func()
 	rclick func()
 	dclick func()
 }
+
+func NewIconFromFile(filePath string) (uintptr, error) {
+	absFilePath, err := filepath.Abs(filePath)
+	if err != nil {
+		return 0, err
+	}
+	hicon, _, _ := LoadImage.Call(
+		0,
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(absFilePath))),
+		IMAGE_ICON,
+		0,
+		0,
+		LR_DEFAULTSIZE|LR_LOADFROMFILE)
+	if hicon == 0 {
+		return 0, errors.New("load image failed: " + filePath)
+	}
+	return hicon, nil
+}
+
+func RegisterWindow(name string, proc WindowProc) error {
+	hinst, _, _ := GetModuleHandle.Call(0)
+	if hinst == 0 {
+		return errors.New("get module handle failed")
+	}
+	hicon, _, _ := LoadIcon.Call(0, uintptr(IDI_APPLICATION))
+	if hicon == 0 {
+		return errors.New("load icon failed")
+	}
+	hcursor, _, _ := LoadCursor.Call(0, uintptr(IDC_ARROW))
+	if hcursor == 0 {
+		return errors.New("load cursor failed")
+	}
+
+	var wc WNDCLASSEX
+	wc.CbSize = uint32(unsafe.Sizeof(wc))
+	wc.LpfnWndProc = syscall.NewCallback(proc)
+	wc.HInstance = HINSTANCE(hinst)
+	wc.HIcon = HICON(hicon)
+	wc.HCursor = HCURSOR(hcursor)
+	wc.HbrBackground = COLOR_BTNFACE + 1
+	wc.LpszClassName = syscall.StringToUTF16Ptr(name)
+
+	atom, _, _ := RegisterClassEx.Call(uintptr(unsafe.Pointer(&wc)))
+	if atom == 0 {
+		return errors.New("register class failed")
+	}
+	return nil
+}
+
+type WindowProc func(hwnd HWND, msg uint32, wparam, lparam uintptr) uintptr
 
 type NOTIFYICONDATA struct {
 	CbSize           uint32
@@ -239,10 +356,14 @@ const (
 	NIM_ADD = 0x00000000
 	NIM_MODIFY = 0x00000001
 	NIM_DELETE = 0x00000002
+	NIM_SETVERSION = 0x00000004
 
 	NIF_MESSAGE = 0x00000001
 	NIF_ICON = 0x00000002
 	NIF_TIP = 0x00000004
+	NIF_STATE = 0x00000008
+
+	NIS_HIDDEN = 0x00000001
 
 	IMAGE_BITMAP = 0
 	IMAGE_ICON = 1
@@ -251,28 +372,43 @@ const (
 
 	IDC_ARROW = 32512
 	COLOR_WINDOW = 5
+	COLOR_BTNFACE = 15
+
+	GWLP_USERDATA = -21
+	WS_CLIPSIBLINGS = 0X04000000
+	WS_EX_CONTROLPARENT = 0X00010000
+
+	HWND_MESSAGE = ^HWND(2)
+	NOTIFYICON_VERSION = 3
 
 	IDI_APPLICATION = 32512
+	WM_APP = 32768
+	NotifyIconMessageId = WM_APP + iota
 )
 
 var (
 	kernel32 = syscall.MustLoadDLL("kernel32")
 	GetModuleHandle = kernel32.MustFindProc("GetModuleHandleW")
 	GetConsoleWindow = kernel32.MustFindProc("GetConsoleWindow")
+	GetLastError = kernel32.MustFindProc("GetLastError")
 
 	shell32 = syscall.MustLoadDLL("shell32.dll")
 	Shell_NotifyIcon = shell32.MustFindProc("Shell_NotifyIconW")
 
 	user32 = syscall.MustLoadDLL("user32.dll")
+	
 	GetMessage = user32.MustFindProc("GetMessageW")
+	IsDialogMessage = user32.MustFindProc("IsDialogMessageW")
 	TranslateMessage = user32.MustFindProc("TranslateMessage")
 	DispatchMessage= user32.MustFindProc("DispatchMessageW")
+	
 	ShowWindow = user32.MustFindProc("ShowWindow")
 	UpdateWindow = user32.MustFindProc("UpdateWindow")
 	DefWindowProc = user32.MustFindProc("DefWindowProcW")
 	RegisterClassEx = user32.MustFindProc("RegisterClassExW")
 	GetDesktopWindow = user32.MustFindProc("GetDesktopWindow")
 	CreateWindowEx = user32.MustFindProc("CreateWindowExW")
+
 	LoadImage = user32.MustFindProc("LoadImageW")
 	LoadIcon = user32.MustFindProc("LoadIconW")
 	LoadCursor = user32.MustFindProc("LoadCursorW")
